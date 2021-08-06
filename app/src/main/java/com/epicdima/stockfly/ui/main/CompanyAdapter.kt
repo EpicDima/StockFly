4dc14880e4af15cd5bfc0763e1af3af1f3676bc0
EpicDima <dima2-8@bk.ru>
2021-08-06 12:24:56 +0300

    Небольшое улучшение разметки

--- app/src/main/java/com/epicdima/stockfly/ui/main/CompanyAdapter.kt
+++ app/src/main/java/com/epicdima/stockfly/ui/main/CompanyAdapter.kt
@@ -94,7 +94,7 @@ class CompanyAdapter(
             binding.apply {
                 root.setCardBackgroundColor(companyItem.rootCardBackgroundColor)
                 ticker.text = companyItem.ticker
-                favouriteIcon.setImageDrawable(companyItem.favouriteIcon)
+                ticker.setCompoundDrawablesWithIntrinsicBounds(null, null, companyItem.favouriteIcon, null)
                 name.text = companyItem.name
                 current.text = companyItem.currentString
                 change.text = companyItem.changeString
@@ -129,7 +129,7 @@ class CompanyAdapter(
                     name.text = companyItem.name
                 }
                 if (payload.favouriteIcon) {
-                    favouriteIcon.setImageDrawable(companyItem.favouriteIcon)
+                    ticker.setCompoundDrawablesWithIntrinsicBounds(null, null, companyItem.favouriteIcon, null)
                 }
                 if (payload.currentString) {
                     current.text = companyItem.currentString
